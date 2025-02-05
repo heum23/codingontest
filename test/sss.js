9c2d414f7caeaca5b0b6c2a40c451f289959ea6a
heum23 <jinsoon6230@naver.com>
2025-02-05 14:18:31 +0900

    12

---
+++ test/sss.js
@@ -0,0 +1,10 @@
+let box1 = document.querySelector(".one");
+let box2 = document.querySelector(".two");
+let box3 = document.querySelector(".three");
+let box4 = document.querySelector(".four");
+const img = document.querySelector(".img");
+console.log(img.innerHTML);
+const changeImg = (id) => {
+  if (id === "one") {
+  }
+};
